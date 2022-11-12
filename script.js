2ab971259b4a02b235f45470d0e9aac9d0224b5b
Rhian Code <88849420+rhian-code@users.noreply.github.com>
2022-11-12 11:02:42 -0300

    Fix

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-$('main h1, main p').fadeOut()
-$('main h1, main p').fadeIn(1600)
-$('.logo, header ul').fadeOut()
-$('.logo, header ul').fadeIn(1600)
+
 let btn = document.querySelector('.btn-menu')
 let menu = document.querySelector('#list-menu')
 btn.addEventListener('click', () => {
