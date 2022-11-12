c4ca35280cb559363924f964b0b23765ae61fd6b
Rhian Code <88849420+rhian-code@users.noreply.github.com>
2022-11-12 10:21:31 -0300

    Att

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ $('main h1, main p').fadeOut()
 $('main h1, main p').fadeIn(1600)
 $('.logo, header ul').fadeOut()
 $('.logo, header ul').fadeIn(1600)
-$('.btn-menu').fadeOut()
-$('.btn-menu').fadeIn(1600)
 let btn = document.querySelector('.btn-menu')
 let menu = document.querySelector('#list-menu')
-$('.btn-menu').text("Menu")
-$('.btn-menu').toggle()
-
-
 btn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
